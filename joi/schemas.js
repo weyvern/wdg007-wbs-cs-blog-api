4d0e5410152e2b5aa76f5477b75f13955d65b7f5
Jorge Paul <jserranopaul@gmail.com>
2022-12-06 18:34:58 +0100

    Starter

---
+++ joi/schemas.js
@@ -0,0 +1,20 @@
+import Joi from 'joi';
+
+export const userSchema = Joi.object({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().required(),
+  password: Joi.string().alphanum().min(8).max(12).required()
+});
+
+export const siginSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().alphanum().min(8).max(12).required()
+});
+
+export const postSchema = Joi.object({
+  title: Joi.string().required(),
+  image: Joi.string().required(),
+  body: Joi.string().required(),
+  author: Joi.string()
+});
